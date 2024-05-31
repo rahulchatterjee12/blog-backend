f5525cbdd0ead80f4777c7634818873362202bad
rahulchatterjee12 <rc5292684@gmail.com>
2024-05-31 21:43:36 +0530

    update code

--- account/serializer.py
+++ account/serializer.py
@@ -13,3 +13,11 @@ class RegisterSerializer(serializers.Serializer):
             raise serializers.ValidationError("username is taken")
 
         return data
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+            username=validated_data["username"],
+        )
+        user.set_password(validated_data["password"])
