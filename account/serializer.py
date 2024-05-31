926d53223c49f719a32157030b116e84530f6e5b
rahulchatterjee12 <rc5292684@gmail.com>
2024-05-31 21:42:38 +0530

    first commit

---
+++ account/serializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class RegisterSerializer(serializers.Serializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        if User.objects.filter(username=data["username"]).exists():
+            raise serializers.ValidationError("username is taken")
+
+        return data
---
+++ account/views.py
@@ -0,0 +1,38 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializer import RegisterSerializer
+
+
+class RegisterView(APIView):
+
+    def post(sef, request):
+        try:
+            data = request.data
+            serializer = RegisterSerializer(data=data)
+
+            if not serializer.is_valid():
+                return Response(
+                    {
+                        "data": serializer.errors,
+                        "message": "something went wrong",
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            serializer.save()
+
+            return Response(
+                {"data": serializer.data, "message": "your account is created"},
+                status=status.HTTP_201_CREATED,
+            )
+
+        except Exception as e:
+            return Response(
+                {
+                    "data": {},
+                    "message": "something went wrong",
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
